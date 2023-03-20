803bc8567a95362b789dca2bf92cd1997659845f
Sebastian Barcelona <sbarcelona@gmail.com>
2023-03-20 11:43:15 -0300

    add changes on fonts

---
+++ src/components/atoms/Loader/styles.ts
@@ -0,0 +1,23 @@
+import styled, { css, keyframes } from 'styled-components'
+
+const pulse = keyframes`
+  0% { transform: scale(1); }
+  60% { transform: scale(1.1); }
+  100% { transform: scale(1); }
+`
+
+export const Wrapper = styled.div`
+  pointer-events: none;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+`
+
+export const AnimatedImg = styled.div`
+  animation: ${pulse} 800ms linear infinite;
+  & > * {
+    width: 72px;
+  }
+`
