a1b53d05563b1686d67d7774fdbc91f51ada4ec3
Jonathan J <ioni_dev@protonmail.com>
2023-04-04 12:53:28 -0300

    grammar fix for jsDocs

--- src/components/atoms/Toggle/index.tsx
+++ src/components/atoms/Toggle/index.tsx
@@ -9,7 +9,7 @@ import { ToggleProps, Variant } from './types';
   @param {Object} props - The component props.
   @param {boolean} props.isActive - The current active state of the toggle switch.
   @param {Function} props.toggle - The function to be called when the toggle switch is clicked.
-  @param {Variant} [props.variant='default'] - The variant of the toggle switch. The optios are 'default', 'theme-switcher' and 'inverted-colors, if you don't pass any it will be default.
+  @param {Variant} [props.variant='default'] - The variant of the toggle switch. The available options are 'default', 'theme-switcher' and 'inverted-colors, if you don't pass any it will be default.
   @param {string} [props.labelText=''] - The label text to be displayed next to the toggle switch.
   @return {JSX.Element}
 */
