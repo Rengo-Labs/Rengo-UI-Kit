d8e9e0a50b5c94447c7992648fbd9a9956898d80
Jonathan J <ioni_dev@protonmail.com>
2023-04-05 10:48:43 -0300

    jsdoc added

--- src/components/atoms/Slider/index.tsx
+++ src/components/atoms/Slider/index.tsx
@@ -5,6 +5,13 @@ interface SliderProps {
   callback: (selectedPercentage: number) => void
 }
 
+/**
+ * The Slider component allows the user to select a percentage value by dragging a slider
+ *
+ * @param {SliderProps} props - The props passed to the Slider component
+ * @param {function} props.callback - {(selectedPercentage: number) => void} - Callback function called when the percentage value changes
+ * @returns {JSX.Element} - A React element representing the Slider component
+*/
 export const Slider = ({ callback }: SliderProps) => {
   const [value, setValue] = useState(50);
 
