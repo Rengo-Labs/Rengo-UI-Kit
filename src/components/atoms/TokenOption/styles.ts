55480a0e500ede9f8bdb34b4b5d2035ddccb95c2
Sebastian Barcelona <sbarcelona@gmail.com>
2023-04-25 16:15:58 -0300

    add some changes to receive the props and update the component

--- src/components/atoms/TokenOption/styles.ts
+++ src/components/atoms/TokenOption/styles.ts
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 type TOption = {
-  isOption1: boolean;
+    isOption1: boolean;
 }
 
 
@@ -14,22 +14,33 @@ export const WrapperStyled = styled.div`
   padding: 5px;
   border-radius: 50px;
   gap: 5px;
-  background-color: ${({ theme }) => theme.color.tokenOption.tokenOptionBackgroundColor};
-  `;
+  background-color: ${({theme}) => theme.color.tokenOption.tokenOptionBackgroundColor};
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  &:active {
+    background-color: ${({theme}) => theme.background.default};
+  }
+`;
 
 export const TokenStyled = styled.div`
-font-family: ${({ theme }) => theme.typography.secondaryFont};
-font-style: normal;
-font-weight: 600;
-font-size: 18px;
-line-height: 26px;
-letter-spacing: 0.02em;
-color: ${({ theme }) => theme.color.tokenOption.tokenOptionTextColor};
+  font-family: ${({theme}) => theme.typography.secondaryFont};
+  font-style: normal;
+  font-weight: 600;
+  font-size: 18px;
+  line-height: 26px;
+  letter-spacing: 0.02em;
+  color: ${({theme}) => theme.color.tokenOption.tokenOptionTextColor};
 `;
 
 export const Ellipse = styled.div<TOption>`
   width: 13px;
   height: 13px;
   border-radius: 50%;
-  background-color: ${({ theme, isOption1 }) => (isOption1 ? theme.color.tokenOption.tokenOption1Color : theme.color.tokenOption.tokenOption2Color)};
+  background-color: ${({
+                         theme,
+                         isOption1
+                       }) => (isOption1 ? theme.color.tokenOption.tokenOption1Color : theme.color.tokenOption.tokenOption2Color)};
 `
