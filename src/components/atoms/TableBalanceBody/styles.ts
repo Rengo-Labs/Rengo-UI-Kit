5d68e96ec30531df14b2d5ea53fa835419a2369b
Jonathan J <ioni_dev@protonmail.com>
2023-05-04 11:32:12 -0300

    first column text overflow wrap responsiveness

--- src/components/atoms/TableBalanceBody/styles.ts
+++ src/components/atoms/TableBalanceBody/styles.ts
@@ -16,12 +16,13 @@ export const TD = styled.td<{ isCryptoColumn: boolean, cryptoColumnWidth: number
   flex: 1;
   width: ${({ cryptoColumnWidth, isCryptoColumn }) => isCryptoColumn && `${cryptoColumnWidth}px` };
   min-height: 32px;
-  padding: 10px;
+  padding: 10px 0 10px 10px;
   display: flex;
   align-items: center;
   justify-content: ${({ isCryptoColumn }) => isCryptoColumn ? 'flex-start' : 'center' };
   flex-wrap: wrap;
   gap: 2px;
+  overflow-wrap: anywhere;
 `
 
 export const Text = styled.p`
