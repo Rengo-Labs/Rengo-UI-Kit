5cc4ec714e67fb43af97da7cd2a3e645a0208f15
Sebastian Barcelona <sbarcelona@gmail.com>
2023-05-05 13:45:31 -0300

    adding paddings to container settings, because class are missing on compilation

--- src/components/molecules/Settings/styles.ts
+++ src/components/molecules/Settings/styles.ts
@@ -4,6 +4,8 @@ import { Column } from '../../atoms';
 export const Wrapper = styled(Column)`
   background-color: ${({ theme }) => theme.color.tab.background};
   border-radius: 16px;
+  padding-left: 2rem;
+  padding-right: 2rem;
 `;
 
 export const Text = styled.p`
