d34cf22ef1e6b8b5d9d925df4fbbecc5bec20c05
Raul Villca <raul.v@hotmail.com.ar>
2023-06-12 12:27:12 -0300

    it adds fix for setting and lpcontainer

--- lib/components/atoms/SettingOption/index.d.ts
+++ lib/components/atoms/SettingOption/index.d.ts
@@ -1,8 +1,8 @@
 import React from 'react';
 export interface SettingOptionProps {
-    value: number;
+    value: string;
     isInput?: boolean;
-    handleValue: (value: number) => void;
+    handleValue: (value: string) => void;
     isSelect?: boolean;
 }
 export declare const SettingOption: ({ value, handleValue, isInput, isSelect }: SettingOptionProps) => React.JSX.Element;
