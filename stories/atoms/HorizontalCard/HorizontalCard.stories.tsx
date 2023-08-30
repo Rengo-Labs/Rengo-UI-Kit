e819cd2cac04c1c19bb10de4ed98c02e8631df21
Raul Villca <raul.villcasd@gmail.com>
2023-08-30 10:27:56 -0300

    it adds label N/A in APR field for stake without gauges

--- stories/atoms/HorizontalCard/HorizontalCard.stories.tsx
+++ stories/atoms/HorizontalCard/HorizontalCard.stories.tsx
@@ -30,7 +30,8 @@ Default.args = {
     { name: 'Your Pooled (WETH)', value: '0.016286696 WETH' },
     { name: `Your Balance (WCSPR-CST)`, value: '217250.092919301' },
     { name: 'Your Share', value: '0.19%'},
-    { name: 'Lp Stake', value: '200'}
+    { name: 'Lp Stake', value: '200'},
+    { name: 'APR', value: undefined }
   ],
   userPoolInfo: [{title: 'Total liquidity ($)', value: '$ 608K'}, {title: "Your Liquidity ($)", value: `$ 22`}],
   trashHandler: () => console.log('horizontal card: delete'),
@@ -43,5 +44,38 @@ Default.args = {
   favoriteHandler: () => console.log('horizontal card: favorite'),
   rewardETHTitle: 'ETH',
   rewardCSTTitle: 'CST',
-  hasLPBalance: true
+  hasLPBalance: true,
+  hasGauge: false
+};
+
+export const LPStake = Template.bind({});
+// @ts-ignore
+LPStake.args = {
+  networkLink: 'https://testnet.cspr.live/contract-package/',
+  contractPackage: 'a3f3a7c26a0723f56ad74dcb4d9a86642d1d53c6d1add00c237df5199a3025e6',
+  firstTokenIcon: csprToken,
+  secondTokenIcon:ethToken,
+  hasFavorite: false,
+  tokenPairs: ['ETH', 'CSPR'],
+  pairsLiquidity: [
+    { name: 'Your Pooled (WCSPR)', value: '1543.804256310 WCSPR' },
+    { name: 'Your Pooled (WETH)', value: '0.016286696 WETH' },
+    { name: `Your Balance (WCSPR-CST)`, value: '217250.092919301' },
+    { name: 'Your Share', value: '0.19%'},
+    { name: 'Lp Stake', value: '200'},
+    { name: 'APR', value: '0.22' }
+  ],
+  userPoolInfo: [{title: 'Total liquidity ($)', value: '$ 608K'}, {title: "Your Liquidity ($)", value: `$ 22`}],
+  trashHandler: () => console.log('horizontal card: delete'),
+  swapHandler: () => console.log('horizontal card: swap'),
+  viewHandler: () => console.log('horizontal card: view'),
+  stakeHandler: () => console.log('horizontal card: stake'),
+  unstakeHandler: () => console.log('horizontal card: unstake'),
+  claimHandler: () => console.log('horizontal card: claim'),
+  addLiquidityHandler: () => console.log('horizontal card: add liquidity'),
+  favoriteHandler: () => console.log('horizontal card: favorite'),
+  rewardETHTitle: 'ETH',
+  rewardCSTTitle: 'CST',
+  hasLPBalance: true,
+  hasGauge: true
 };
