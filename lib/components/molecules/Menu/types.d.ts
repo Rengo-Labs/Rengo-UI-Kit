6812968fed75345248a82f88fad021b3c1196d54
Raul Villca <raul.villcasd@gmail.com>
2024-02-04 20:06:56 -0300

    it uploads test lib

--- lib/components/molecules/Menu/types.d.ts
+++ lib/components/molecules/Menu/types.d.ts
@@ -6,14 +6,13 @@ export interface PageLink {
     path: string;
     onAction: any;
 }
-export interface OptAction {
+export interface OptActionProps {
     startIcon: any;
     endIcon: any;
     title: string;
     background: string;
     color: string;
     onAction: () => void;
-    onActionConnected?: () => void;
     isWalletConnected?: boolean;
     walletAddress?: string;
 }
@@ -23,7 +22,8 @@ export interface MenuPros {
     title: string;
     links: PageLink[];
     toggle?: ToggleProps;
-    rightAction?: OptAction;
+    rightOptionAction: OptActionProps;
+    rightConnectionAction: OptActionProps;
     children?: React.ReactNode;
     ref?: React.ForwardedRef<HTMLDivElement>;
     handleRedirect?: () => void;
